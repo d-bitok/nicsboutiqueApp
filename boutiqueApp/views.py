de6a7fa5a74288710c085a96f48537af63c9aca0
rexdougie <bitokdouglas@gmail.com>
2020-11-05 12:32:20 +0000

    Customer Error

--- boutiqueApp/views.py
+++ boutiqueApp/views.py
@@ -45,7 +45,7 @@ def updateItem(request):
 
 def store(request):
     if request.user.is_authenticated:
-        customer = request.user.Customer
+        customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
