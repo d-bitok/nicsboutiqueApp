d29def82ae6d6739a52a984daafbb9c7910bbac3
rexdougie <bitokdouglas@gmail.com>
2020-11-05 11:43:35 +0000

    Added JS functionality

--- boutiqueApp/models.py
+++ boutiqueApp/models.py
@@ -1,3 +1,4 @@
+from posix import times_result
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -61,6 +62,15 @@ class Order(models.Model):
         orderitems = self.orderitem_set.all()
         total = sum([item.quantity for item in orderitems])
         return total
+
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems= self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+        return shipping
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL,blank=True, null=True)
--- boutiqueApp/views.py
+++ boutiqueApp/views.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import User
+from django.http import request
 from django.shortcuts import render, get_object_or_404
 from .models import *
+import json
+import datetime
+from django.shortcuts import render
+from django.http import JsonResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -12,11 +17,46 @@ def home(request):
     }
     return render(request, 'boutiqueApp/home.html', context)
 
-class ProductListView(ListView):
-    model = Product
-    template_name = 'boutiqueApp/store.html'
-    context_object_name = 'products'
-    paginate_by = 6
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('Action:', action)
+    print('Product:', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse('Item was added', safe=False)
+
+def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+        cartItems = order['get_cart_items']
+
+    products = Product.objects.all()
+    context = {'products':products, 'cartItems':cartItems}
+    return render(request, 'boutiqueApp/store.html', context)
 
 class PostListView(ListView):
     model = Post
@@ -79,29 +119,63 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+        cartItems = order['get_cart_items']
 
     context = {
         'items' : items,
         'order' : order,
-        'title':'Cart'
+        'title':'Cart',
+        'cartItems': cartItems
     }
     return render(request, 'boutiqueApp/cart.html', context)
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address=data['shipping']['adress'],
+                city=data['shipping']['city'],
+                state=data['shipping']['state'],
+                zipcode=data['shipping']['zipcode'],
+            )
+
+    else:
+        print('Unauthenticted User! Log In.')
+    return JsonResponse('Payment Complete!', safe=False)
 
 def checkout(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+        cartItems = order['get_cart_items']
 
     context = {
         'items' : items,
         'order' : order,
-        'title':'Checkout'
+        'title':'Checkout',
+        'cartItems': cartItems
     }
     return render(request, 'boutiqueApp/checkout.html', context)
