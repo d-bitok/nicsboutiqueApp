7570263cd9abdd4c09c6a971687738a65f3ae381
rexdougie <bitokdouglas@gmail.com>
2020-11-10 10:51:30 +0000

    Integrity Error, designer_id

--- boutiqueApp/signals.py
+++ boutiqueApp/signals.py
@@ -8,9 +8,9 @@ def create_product(sender, instance, created, **kwargs):
     if created:
         Product.objects.create(user=instance)
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=Product)
 def save_product(sender, instance, **kwargs):
-    instance.name.save()
+    instance.productName.save()
 
 @receiver(post_save, sender=Product)
 def customer(sender, instance, created, **kwargs):
