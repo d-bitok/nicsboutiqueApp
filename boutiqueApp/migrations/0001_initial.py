1e547155cb5f6cb578718483b626e54d055aa071
rexdougie <bitokdouglas@gmail.com>
2020-11-10 12:44:03 +0000

    Integrity Error, designer_id

--- boutiqueApp/migrations/0001_initial.py
+++ boutiqueApp/migrations/0001_initial.py
@@ -65,7 +65,7 @@ class Migration(migrations.Migration):
                 ('date_added', models.DateTimeField(default=django.utils.timezone.now)),
                 ('image', models.ImageField(default='knot.jpeg', upload_to='market')),
                 ('description', models.CharField(max_length=300, null=True)),
-                ('designer', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='boutiqueApp.designer')),
+                #('designer', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='boutiqueApp.designer')),
             ],
         ),
         migrations.CreateModel(
