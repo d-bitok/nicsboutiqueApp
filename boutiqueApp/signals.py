a559d3dc79b34c8c5c6672775c3411c5280fb111
rexdougie <bitokdouglas@gmail.com>
2020-11-11 08:40:00 +0000

    Product()

--- boutiqueApp/signals.py
+++ boutiqueApp/signals.py
@@ -1,15 +1,15 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from .models import Product, Seller
+from .models import Product
 
 @receiver(post_save, sender=User)
 def create_product(sender, instance, created, **kwargs):
     if created:
         Product.objects.create(user=instance)
 
-@receiver(post_save, sender=Product)
-def save_profile(sender, instance, **kwargs):
+@receiver(post_save, sender=User)
+def save_product(sender, instance, **kwargs):
     try:
         instance.product.save()
     except:
